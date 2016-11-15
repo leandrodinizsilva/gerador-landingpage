f2cd63dc0d69ae1b7c8e4d213e39d4efbd9270af
Jonathan Messias <jonathan.tif@gmail.com>
2016-11-15 10:44:50 -0200

    update funcionando de rodapé

--- admin/rodape.php
+++ admin/rodape.php
@@ -10,54 +10,75 @@ if (1 == 1) {
             <i class='glyphicon glyphicon-menu-right'></i> Rodapé
         </li>
     </ol>
-    <!-- <form method="post" enctype="multipart/form-data"> -->
     <div class="box col-md-4">
         <div class="panel panel-primary shadow">
             <div class="panel-heading margin-header"><i class='glyphicon glyphicon-th-list'></i> &nbsp;Template Selecionado</div>
             <div class="padding-interno">
-               <table class="table" style="margin-bottom:0">
+             <table class="table" style="margin-bottom:0">
                 <tbody>
                     <tr>
                         <td>
                             <div class="form-group">
-                                <h4>Teste</h4>
+                                <?php
+                                if ( isset($titulo_ativo) ) {
+                                    echo "<h4>{$titulo_ativo}</h4>";
+                                } else {
+                                    echo "<h4>Nenhum template selecionado!</h4>";
+                                }
+                                ?>
                             </div>
                             <div class="form-group">
-                                <a href="bloco4.php" class="btn btn-block btn-warning"><< Anterior</a>
+                                <?php
+                                if ( isset($_GET['retorno']) ) {
+                                    if ($_GET['retorno'] === '1') {
+                                        echo $DB->msg('Sucesso!', 'success');
+                                    } else {
+                                        echo $DB->msg('Falha!', 'danger');
+                                    }
+                                }
+                                ?>
+                                <?php
+                                if ( isset($_GET['id_bloco4']) ) {
+                                    echo "<a href='bloco4.php?id_bloco4=1' class='btn btn-block btn-warning'><< Anterior</a>";
+                                } else {
+                                    echo "<a href='bloco4.php' class='btn btn-block btn-warning'><< Anterior</a>";
+                                }
+                                ?>
                             </div>
                         </td>
                     </tr>
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+            </table>
         </div>
     </div>
-    <div class="box col-md-8">
-        <div class="panel panel-primary shadow">
-            <div class="panel-heading margin-header"><i class='glyphicon glyphicon-th-list'></i> &nbsp;Rodapé</div>
-            <div class="padding-interno">
-               <table class="table" style="margin-bottom:0">
+</div>
+<div class="box col-md-8">
+    <div class="panel panel-primary shadow">
+        <div class="panel-heading margin-header"><i class='glyphicon glyphicon-th-list'></i> &nbsp;Rodapé</div>
+        <div class="padding-interno">
+            <form method="post" enctype="multipart/form-data">
+             <table class="table" style="margin-bottom:0">
                 <tbody>
                     <tr>
                         <td>
                             <div class="form-group">
                                 <label>Texto</label>
-                                <input type="text" name="texto" value="<?php echo ( isset($titulo_e) ) ? $titulo_e : $titulo ?>" maxlength="20" class="form-control" />
+                                <input type="text" name="texto_rodape" value="<?php echo $texto_rodape ?>" maxlength="20" class="form-control" />
                             </div>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <div class="form-group">
-                                <input type='submit' class='btn btn-block btn-success' name='salvar' value='Salvar' />
+                                <input type='submit' class='btn btn-block btn-success' name='update_rodape' value='Salvar' />
                             </td>
                         </tr>
                     </tbody>
                 </table>
-            </div>
+            </form>
         </div>
     </div>
-</div>
+    </div>
 <?php
 include('inc/footer.php');
 } else {
